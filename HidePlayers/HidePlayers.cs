64372f9f278819cb6c99643d0dc02700596c1045
NytroPenguin <gcruice@yahoo.com>
2018-08-15 20:43:58 -0500

    hide pets

--- HidePlayers/HidePlayers.cs
+++ HidePlayers/HidePlayers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 using System.Collections.Generic;
 using Lib_K_Relay;
 using Lib_K_Relay.Utilities;
@@ -8,11 +9,13 @@ using Lib_K_Relay.Networking.Packets;
 using Lib_K_Relay.Networking.Packets.Client;
 using Lib_K_Relay.Networking.Packets.Server;
 using Lib_K_Relay.Networking.Packets.DataObjects;
+using Lib_K_Relay.GameData;
 namespace HidePlayers
 {
     public class HidePlayers : IPlugin
     {
-        private bool _size = false;
+        private bool _hidePlayers = false;
+        private List<int> pets = new List<int>();
         public string GetAuthor()
         {
             return "NytroPenguin";
@@ -37,6 +40,16 @@ namespace HidePlayers
         {
             proxy.HookCommand("hideplayers", onCommand);
             proxy.HookPacket(PacketType.UPDATE, onUpdate);
+
+            foreach (var obj in GameData.Objects.Map)
+            {
+                if (obj.Value.Pet)
+                {
+                    pets.Add(obj.Value.ID);
+                    PluginUtils.Log("PetID", obj.Value.ID.ToString());
+                }
+            }
+            PluginUtils.Log("test:", pets.Count.ToString() + " pets loaded");
         }
 
 
@@ -45,21 +58,22 @@ namespace HidePlayers
             UpdatePacket update = (UpdatePacket)packet;
             foreach (Entity obj in update.NewObjs)
             {
-                if (Enum.IsDefined(typeof(Classes), (short)obj.ObjectType) && obj.Status.ObjectId != client.ObjectId)
+                if ((Enum.IsDefined(typeof(Classes), (short)obj.ObjectType) && obj.Status.ObjectId != client.ObjectId) || (pets.Contains(obj.ObjectType)))
                 {
                     foreach (var data in obj.Status.Data)
                     {
-                        if (data.Id == StatsType.Size && _size)
+                        if (data.Id == StatsType.Size && _hidePlayers)
                             data.IntValue = 0;
                     }
                 }
+
             }
         }
 
         private void onCommand(Client client, string command, string[] args)
         {
-            if (args[0] == "on") _size = true;
-            else if (args[0] == "off") _size = false;
+            if (args[0] == "on") _hidePlayers = true;
+            else if (args[0] == "off") _hidePlayers = false;
             else return;
             client.SendToClient(PluginUtils.CreateNotification(client.ObjectId, "HidePlayers: Size = " + args[0]));
         }
